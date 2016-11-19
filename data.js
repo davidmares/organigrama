b541baf2d1baf92f3ffbc1d9932253bab058d109
David Mares <lost_mares@hotmail.com>
2016-11-18 18:28:26 -0600

    Agregando archivos

---
+++ data.js
@@ -0,0 +1,216 @@
+var data = {
+  "id": 1,
+  "puesto": "Gerente",
+  "nombre": "Nodo 1",
+  "hijos": [
+    {
+      "id": 2,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-1",
+      "hijos": [
+        {
+          "id": 3,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-1-1",
+          "hijos": [
+            {
+              "id": 4,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-1-1-1",
+              "hijos": [
+                {
+                  "id": 5,
+                  "puesto": "Supervisor",
+                  "nombre": "Nodo 1-1-1-1-1",
+                  "hijos": []
+                },
+                {
+                  "id": 6,
+                  "puesto": "Supervisor",
+                  "nombre": "Nodo 1-1-1-1-2",
+                  "hijos": []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "id": 7,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-1-2",
+          "hijos": []
+        },
+        {
+          "id": 8,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-1-3",
+          "hijos": []
+        }
+      ]
+    },
+    {
+      "id": 9,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-2",
+      "hijos": []
+    },
+    {
+      "id": 10,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-3",
+      "hijos": [
+        {
+          "id": 11,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-3-1",
+          "hijos": [
+            {
+              "id": 12,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-3-1-1",
+              "hijos": []
+            },
+            {
+              "id": 13,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-3-1-2",
+              "hijos": [
+                {
+                  "id": 14,
+                  "puesto": "Supervisor",
+                  "nombre": "Nodo 1-3-1-2-1",
+                  "hijos": [
+                    {
+                      "id": 15,
+                      "puesto": "Supervisor",
+                      "nombre": "Nodo 1-3-1-2-1-1",
+                      "hijos": [
+                        {
+                          "id": 16,
+                          "puesto": "Supervisor",
+                          "nombre": "Nodo 1-3-1-2-1-1-1",
+                          "hijos": [
+                            {
+                              "id": 17,
+                              "puesto1": "Supervisor",
+                              "nombre": "Nodo 1-3-1-2-1-1-1-1",
+                              "hijos": []
+                            },
+                            {
+                              "id": 18,
+                              "puesto": "Supervisor",
+                              "nombre": "Nodo 1-3-1-2-1-1-1-2",
+                              "hijos": []
+                            }
+                          ]
+                        },
+                        {
+                          "id": 19,
+                          "puesto": "Supervisor",
+                          "nombre": "Nodo 1-3-1-2-1-1-2",
+                          "hijos": []
+                        }
+                      ]
+                    },
+                    {
+                      "id": 20,
+                      "puesto": "Supervisor",
+                      "nombre": "Nodo 1-3-1-2-1-2",
+                      "hijos": []
+                    }
+                  ]
+                },
+                {
+                  "id": 21,
+                  "puesto": "Supervisor",
+                  "nombre": "Nodo 1-3-1-2-2",
+                  "hijos": []
+                }
+              ]
+            },
+            {
+              "id": 22,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-3-1-3",
+              "hijos": []
+            },
+            {
+              "id": 23,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-3-1-4",
+              "hijos": []
+            }
+          ]
+        },
+        {
+          "id": 24,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-3-2",
+          "hijos": []
+        }
+      ]
+    },
+    {
+      "id": 25,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-4",
+      "hijos": []
+    },
+    {
+      "id": 26,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-5",
+      "hijos": []
+    },
+    {
+      "id": 27,
+      "puesto": "Supervisor",
+      "nombre": "Nodo 1-6",
+      "hijos": [
+        {
+          "id": 28,
+          "puesto": "Supervisor",
+          "nombre": "Nodo 1-6-1",
+          "hijos": [
+            {
+              "id": 29,
+              "puesto": "Supervisor",
+              "nombre": "Nodo 1-6-1-1",
+              "hijos": []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+var data2 = {
+  "id": 1,
+  "puesto": "Director",
+  "nombre": "David Mares",
+  "hijos": [{
+    "id": 2,
+    "puesto": "Gerente TI",
+    "nombre": "Gustavo Dias",
+    "hijos": []
+  },
+  {
+    "id": 3,
+    "puesto": "Gerente Gral",
+    "nombre": "Pedro Alonzo",
+    "hijos": []
+  },
+  {
+    "id": 4,
+    "puesto": "Gerente Ventas",
+    "nombre": "Brenda Sanchez",
+    "hijos": []
+  },
+  {
+    "id": 5,
+    "puesto": "Gerente Operacion",
+    "nombre": "Adrian Davila",
+    "hijos": []
+  }]
+}
---
+++ organigrama.js
@@ -0,0 +1,217 @@
+/*
+	Libreria para creación de organigramas
+	Autor: David Mares
+	Dependencias: bootstrap style
+	Version: DEV
+*/
+(function(window, document){
+	var organigrama = function(){
+	};
+
+	organigrama.prototype.create = function(e) {
+		this.tabla = document.createElement('table');
+
+		this.parent = document.getElementById(e);
+		this.parent.innerHTML = '';
+		this.parent.className = 'organigrama';
+		this.parent.appendChild(this.tabla);
+
+		addNode(this.tabla, this.data);
+
+		createControls(this.tabla);
+	};
+
+	organigrama.prototype.eventAdd = function(c){
+		if(c === undefined){
+			alert('Evento Add mal definido');
+			return;
+		}	
+		var btns = this.tabla.querySelectorAll('.nodo .btn-add');
+
+		for(var i = 0; i < btns.length; i++){
+			on('click', btns[i], function(){
+				c(this.getAttribute('data-id'));
+			});
+		}
+	};
+
+	organigrama.prototype.eventEdit = function(c){
+		if(c === undefined){
+			alert('Evento Edit mal definido');
+			return;
+		}	
+		var btns = this.tabla.querySelectorAll('.nodo .btn-edit');
+
+		for(var i = 0; i < btns.length; i++){
+			on('click', btns[i], function(){
+				c(this.getAttribute('data-id'));
+			});
+		}
+	};
+
+	var addNode = function(parent, data) {
+		// Elementos para el nodo principal
+		var row = document.createElement('tr');
+		var cell = document.createElement('td');
+		var nodo = document.createElement('div');
+		nodo.className = 'nodo';
+		nodo.setAttribute('data-id', data.id);
+		row.className = 'nodo-header';
+
+		if(data != undefined){
+			// contador de hijos
+			var childs = data.hijos != undefined ? data.hijos.length * 2 : 0;
+
+			cell.setAttribute('colspan', childs * 2);
+			var item = document.createElement('div');
+			item.innerText = data.id;
+			item.className = 'nodo-id badge';
+			nodo.appendChild(item);
+
+			item = document.createElement('div');
+			item.innerText = data.puesto;
+			item.className = 'nodo-puesto';
+			nodo.appendChild(item);
+
+			item = document.createElement('div');
+			item.innerText = data.nombre;
+			item.className = 'nodo-nombre';
+			nodo.appendChild(item);
+
+			cell.appendChild(nodo);
+			row.appendChild(cell);
+			parent.appendChild(row);
+
+			// ya se agrego el nodo principal, se agregan los hijos
+			if(childs != 0){
+
+				var btnExpand = document.createElement('div');
+				btnExpand.className = 'btn-collapse glyphicon glyphicon-minus';
+				nodo.appendChild(btnExpand);
+
+				on('click', btnExpand, expandCollapse);
+
+				var trs = document.createElement('tr');
+				var tds = document.createElement('td');
+				var div = document.createElement('div');
+				trs.className = 'nodo-child';
+				div.className = 'line-down';
+				div.innerHTML = '&nbsp;';
+
+				tds.appendChild(div);
+				tds.setAttribute('colspan', childs );
+				trs.appendChild(tds);
+				parent.appendChild(trs);
+
+				trs = document.createElement('tr');
+				trs.className = 'nodo-child';
+				
+				for(var i = 0; i < childs; i++){
+					tds = document.createElement('td');
+					tds.className = 'line ';
+					tds.innerHTML = '&nbsp;';
+					if(childs == 2){
+						if(i%2==0)
+							tds.className += 'right';
+						else
+							tds.className += 'left';
+					}
+					if(i != 0 && i != childs - 1){
+						if(i%2==0)
+							tds.className += 'right up';
+						else
+							tds.className += 'left up';
+					}
+					trs.appendChild(tds);
+				}
+
+				parent.appendChild(trs);
+
+				trs = document.createElement('tr');
+				trs.className = 'nodo-child';
+				parent.appendChild(trs);
+
+				for(var i = 0; i < childs/2; i++){
+					tds = document.createElement('td');
+					tds.setAttribute('colspan', 2);
+
+					trs.appendChild(tds);
+
+					var child = document.createElement('table');
+
+					tds.appendChild(child);
+
+					addNode(child, data.hijos[i]);
+				}
+			}
+		}		
+		else{
+			nodo.innerText = 'Empty';
+			cell.appendChild(nodo);
+			row.appendChild(cell);
+			parent.appendChild(row);
+		}
+	};
+
+	var createControls = function(tabla){
+		
+		var nodos = tabla.querySelectorAll('.nodo');
+		for(var i = 0; i < nodos.length; i++){
+			var id = nodos[i].getAttribute('data-id');
+			var div = document.createElement('div');
+			div.className = 'controls';
+
+			var btn = document.createElement('btn');
+			btn.setAttribute('type','button');
+			btn.setAttribute('data-id', id);
+			btn.className = 'btn btn-primary btn-block btn-add';
+
+			var icon = document.createElement('i');
+			icon.className = 'glyphicon glyphicon-plus';
+			btn.appendChild(icon);
+			div.appendChild(btn);
+
+			btn = document.createElement('btn');
+			btn.setAttribute('type','button');
+			btn.setAttribute('data-id', id);
+			btn.className = 'btn btn-success btn-block btn-edit';
+
+			icon = document.createElement('i');
+			icon.className = 'glyphicon glyphicon-pencil';
+			btn.appendChild(icon);
+			div.appendChild(btn);
+
+			nodos[i].appendChild(div);
+		}
+	};
+
+	function expandCollapse(){
+		var table = undefined;
+		if(this.parentNode)
+			table = this.parentNode.parentNode.parentNode.parentNode;
+		else
+			table = this.parent.parent.parent.parent;
+
+		var childs = table.querySelectorAll('.nodo-child');
+		for(var i = 0; i < childs.length; i++){
+			var classname = childs[i].className;
+			childs[i].className = classname.indexOf('hidden') != -1 ?
+				classname.replace(' hidden','') :
+				classname + ' hidden';
+		}
+		if(childs.length > 1)
+			this.className = this.className.indexOf('minus') != -1 ?
+				this.className.replace('minus', 'plus') :
+				this.className.replace('plus', 'minus');
+	};
+
+	var on = function(e, i, c){
+		if(document.addEventListener)
+			i.addEventListener(e, c, false);
+		else
+			i.attachEvent('on' + e, c);
+	}
+
+	window.organigrama = new organigrama();
+		
+})(window, document);
